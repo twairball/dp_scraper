916ecdaaf86b49a71058cf83772be789c944228a
Yuchao <543940962@qq.com>
2016-01-05 13:46:11 +0800

    scrape coupon

--- lib/dp_scraper/models/coupon.rb
+++ lib/dp_scraper/models/coupon.rb
@@ -1,14 +1,11 @@
 class DpScraper::Coupon
 
-	attr_reader :title, :description
+	attr_reader :title, :description, :limit_time
 
-	def initialize(title: nil, description: nil)
+	def initialize(title: nil, description: nil, limit_time: nil)
 		@title = title
 		@description = description
-
+		@limit_time = limit_time
 	end
-
-
-
-
+	
 end
